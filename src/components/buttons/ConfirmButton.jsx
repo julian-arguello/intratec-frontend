e6f6b19e29f5dcec1457e39bb5a6271c8f1ec0d5
alessandro <ale.signo@yahoo.com.ar>
2023-06-12 18:00:08 -0300

    css fixes in new service screen

---
+++ src/components/buttons/ConfirmButton.jsx
@@ -0,0 +1,11 @@
+export const ConfirmButton = () => {
+  return (
+    <button
+      type="submit"
+      className="btn-confirm d-flex justify-content-center align-items-center"
+    >
+      <span class="material-icons-outlined me-2">check_circle</span>
+      Confirmar
+    </button>
+  );
+};
--- src/pages/services/ServiceForm.jsx
+++ src/pages/services/ServiceForm.jsx
@@ -1,12 +1,28 @@
-import ServiceFormAdd from "../../components/service/ServiceFormAdd"
-import ServiceFormEdit from "../../components/service/ServiceFormEdit";
+import { BackButton } from '../../components/buttons/BackButton';
+import ServiceFormAdd from '../../components/service/ServiceFormAdd';
+import ServiceFormEdit from '../../components/service/ServiceFormEdit';
 
-export function ServiceAdd(props){    
-    return(
-        <main>
-            {!props.edit ? <h2 className="my-5 pt-5 pt-sm-0 text-center text-sm-start">Agregar servicio</h2> : <h2 className="my-5 pt-5 pt-sm-0 text-center text-sm-start">Editar servicio</h2>}
-            {!props.edit ? <ServiceFormAdd /> : <ServiceFormEdit />}
-        </main>
-    )
+export function ServiceAdd(props) {
+  return (
+    <div>
+      {!props.edit ? (
+        <div className="row align-items-center mb-5 mt-3">
+          <h2 className="col-6 col-md-4 text-end text-sm-start h4 order-1 order-md-0 mb-0">
+            Nuevo servicio
+          </h2>
+          <div className="col-6 col-md-4 order-0 order-md-1">
+            <div className="float-md-end">
+              <BackButton />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h2 className="my-5 pt-5 pt-sm-0 text-center text-sm-start">
+          Editar servicio
+        </h2>
+      )}
+      {!props.edit ? <ServiceFormAdd /> : <ServiceFormEdit />}
+    </div>
+  );
 }
-export default ServiceAdd
+export default ServiceAdd;
